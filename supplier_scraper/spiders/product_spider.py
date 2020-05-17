d9d8f79f5a8bc394e7dc72415bccb34fd77a8604
AJ Ferron <alvn.frrn@gmail.com>
2020-05-17 17:52:49 -0400

    Create FormRequest for login

--- supplier_scraper/spiders/product_spider.py
+++ supplier_scraper/spiders/product_spider.py
@@ -5,14 +5,40 @@ from ..items import ProductItem
 # scrapy crawl products
 # scrapy shell -s USER_AGENT='custom user agent' "http://localhost:8080/index.html"
 
+sanmar_login = {
+    'w3exec': 'login', 
+    'customerNo': '27539',
+    'email': 'email',
+    'password': 'pass', 
+    'send': ''
+}
+
 class ProductSpider(scrapy.Spider):
     name = "products"
 
     custom_settings = {
-        "ITEM_PIPELINES": {'supplier_scraper.pipelines.ProductImagePipeline': 1},
         "IMAGES_STORE": 'images',
-        "DOWNLOAD_DELAY": 1
+        "DOWNLOAD_DELAY": 1,
+        "ITEM_PIPELINES": {
+            'supplier_scraper.pipelines.ProductImagePipeline': 100
+
+        }
     }
+
+    def start_requests(self):
+        return [scrapy.FormRequest(
+            url = "https://www.sanmarcanada.com/flashconnect/index/index/",
+            formdata = sanmar_login,
+            callback = self.after_login
+        )]
+
+
+    def after_login(self, response):
+        if 'login' in response.url:
+            self.logger.error("[!] LOGIN FAILED")
+            return
+        
+        return [scrapy.Request(url=url, meta={'product': False}) for url in self.start_urls]
 
     def parse(self, response):
         name = response.css('.product-name h1::text').get()
@@ -20,6 +46,6 @@ class ProductSpider(scrapy.Spider):
         imgs = response.css('#itemslider-zoom')[0]
         urls = imgs.css(".item a::attr(href)").getall()
         clrs = imgs.css('.item a::attr(title)').getall()
-        sku = name.split('.')[1].replace(' ', '')
+        sku = name.split('.')[1].replace(' ', '').lower()
 
         return ProductItem(name=name, sku=sku, description=desc, colors=clrs, image_urls=urls)
